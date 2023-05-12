20cdecf431029caf197ae5b2591fa19292a58f8f
lumi <fatlum.llumnica@gmail.com>
2023-05-12 16:00:49 +0200

    slider added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
+import MovieCard from '../components/MovieCard';
 
 
 function Home() {
 
-    const [slides, setSlides] = useState([])
+    const [movies, setMovies] = useState([])
 
 
     useEffect(() => { 
@@ -21,8 +22,8 @@ function Home() {
         };
 
         axios.request(options).then((response) => {
-            setSlides(response.data);
-            console.log(response.data)
+            setMovies(response.data.results);
+            console.log(response.data.results)
         }).catch((error) => {
             console.log(error)});
         }, [])
@@ -31,18 +32,22 @@ function Home() {
 
   return (
     <>
-
+    <section className='slider'>
     <div id="carouselExample" className="carousel slide">
         <div className="carousel-inner">
-                <div className="carousel-item active">
-                    <img src="..." className="d-block w-100" alt="..."/>
-                </div>
-                <div className="carousel-item">
-                    <img src="..." className="d-block w-100" alt="..."/>
-                </div>
-                <div className="carousel-item">
-                    <img src="..." className="d-block w-100" alt="..."/>
-                </div>
+
+            {
+                movies && movies.map( (movie, index) => {
+                    if(movie.primaryImage !== null) {
+                        return (
+                            <div className={ index === 0 ? 'carousel-item active' : 'carousel-item'} key={movie.id}>
+                                <img src={movie.primaryImage.url} className="d-block w-100" alt="..."/>
+                            </div>
+                        );
+                    }
+                
+                })
+            }
         </div>
         <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
             <span className="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -53,6 +58,17 @@ function Home() {
             <span className="visually-hidden">Next</span>
         </button>
     </div>
+    </section>
+    <section className='latest-movies bg-light py-5'>
+        <div className="container">
+            <h3 className='mb-5 text-center'>Latest movies</h3>
+            <div className="row">
+            {
+                movies && movies.map((movie)=> <div key={movie.id} className='col-lg-3 col-md-4 col-sm-12'><MovieCard  movie={movie}/></div>)
+            }
+            </div>
+        </div>
+    </section>
 
 </>
 )
